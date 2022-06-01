52de1918b4e6e255239b5e0f8824d85626f8d410
mostafa <msk198@usal.edu.lb>
2022-06-02 00:53:54 +0300

    Initial commit

---
+++ lib/sweet_snackbar.dart
@@ -0,0 +1,168 @@
+library sweet_snackbar;
+
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class CustomSnackBar extends StatefulWidget {
+  final String message;
+  final Widget icon;
+  final Color backgroundColor;
+  final TextStyle textStyle;
+  final int iconRotationAngle;
+  final List<BoxShadow> boxShadow;
+  final BorderRadius borderRadius;
+  final double iconPositionTop;
+  final double iconPositionLeft;
+  final EdgeInsetsGeometry messagePadding;
+  final double textScaleFactor;
+
+  const CustomSnackBar.success({
+    Key? key,
+    required this.message,
+    this.messagePadding = const EdgeInsets.symmetric(horizontal: 24),
+    this.icon = const Icon(
+      Icons.check_circle,
+      color: Colors.white,
+    ),
+    this.textStyle = const TextStyle(
+      fontWeight: FontWeight.w600,
+      fontSize: 16,
+      color: Colors.white,
+    ),
+    this.iconRotationAngle = 0,
+    this.iconPositionTop = -0,
+    this.iconPositionLeft = -0,
+    this.backgroundColor = const Color(0xff00E676),
+    this.boxShadow = kDefaultBoxShadow,
+    this.borderRadius = kDefaultBorderRadius,
+    this.textScaleFactor = 1.0,
+  });
+    CustomSnackBar.normal({
+    Key? key,
+    required this.message,
+    this.messagePadding = const EdgeInsets.symmetric(horizontal: 0),
+    this.icon = const Icon(
+      Icons.info_outline,
+      size: 20,
+    ),
+    this.textStyle = const TextStyle(
+      fontWeight: FontWeight.w600,
+      fontSize: 16,
+      color: Colors.white,
+    ),
+    this.iconRotationAngle = 0,
+    this.iconPositionTop = -0,
+    this.iconPositionLeft = -0,
+    this.backgroundColor = Colors.grey,
+    this.boxShadow = kDefaultBoxShadow,
+    this.borderRadius = kDefaultBorderRadius,
+    this.textScaleFactor = 1.0,
+  });
+
+  const CustomSnackBar.info({
+    Key? key,
+    required this.message,
+    this.messagePadding = const EdgeInsets.symmetric(horizontal: 24),
+    this.icon = const Icon(
+      Icons.info_outline,
+      color: Colors.white,
+    ),
+    this.textStyle = const TextStyle(
+      fontWeight: FontWeight.w600,
+      fontSize: 16,
+      color: Colors.white,
+    ),
+    this.iconRotationAngle = 0,
+    this.iconPositionTop = -0,
+    this.iconPositionLeft = -0,
+    this.backgroundColor = const Color(0xff2196F3),
+    this.boxShadow = kDefaultBoxShadow,
+    this.borderRadius = kDefaultBorderRadius,
+    this.textScaleFactor = 1.0,
+  });
+
+  const CustomSnackBar.error({
+    Key? key,
+    required this.message,
+    this.messagePadding = const EdgeInsets.symmetric(horizontal: 24),
+    this.icon = const Icon(
+      Icons.warning,
+      color: Colors.white,
+    ),
+    this.textStyle = const TextStyle(
+      fontWeight: FontWeight.w600,
+      fontSize: 16,
+      color: Colors.white,
+    ),
+    this.iconRotationAngle = 0,
+    this.iconPositionTop = -0,
+    this.iconPositionLeft = -0,
+    this.backgroundColor = const Color(0xffff5252),
+    this.boxShadow = kDefaultBoxShadow,
+    this.borderRadius = kDefaultBorderRadius,
+    this.textScaleFactor = 1.0,
+  });
+
+  @override
+  _CustomSnackBarState createState() => _CustomSnackBarState();
+}
+
+class _CustomSnackBarState extends State<CustomSnackBar> {
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Container(
+      clipBehavior: Clip.hardEdge,
+      height: 52,
+      decoration: BoxDecoration(
+        color: widget.backgroundColor,
+        borderRadius: widget.borderRadius,
+        boxShadow: widget.boxShadow,
+      ),
+      width: double.infinity,
+      child: Stack(
+        children: [
+          Positioned(
+            top: widget.iconPositionTop,
+            left: widget.iconPositionLeft,
+            child: Container(
+              padding: EdgeInsets.only(left: 20),
+              height: 55,
+              child: Transform.rotate(
+                angle: widget.iconRotationAngle * pi / 180,
+                child: widget.icon,
+              ),
+            ),
+          ),
+          Center(
+            child: Padding(
+              padding: widget.messagePadding,
+              child: Text(
+                widget.message,
+                style: theme.textTheme.bodyText2?.merge(
+                  widget.textStyle,
+                ),
+                textAlign: TextAlign.center,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+                textScaleFactor: widget.textScaleFactor,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+const kDefaultBoxShadow = const [
+  BoxShadow(
+    color: Colors.black26,
+    offset: Offset(0.0, 8.0),
+    spreadRadius: 1,
+    blurRadius: 30,
+  ),
+];
+
+const kDefaultBorderRadius = const BorderRadius.all(Radius.circular(12));
---
+++ lib/top_snackbar.dart
@@ -0,0 +1,176 @@
+import 'package:flutter/material.dart';
+import 'package:sweet_snackbar/top_bounce.dart';
+
+OverlayEntry? _previousEntry;
+
+/// Displays a widget that will be passed to [child] parameter above the current
+/// contents of the app, with transition animation
+///
+/// The [child] argument is used to pass widget that you want to show
+///
+/// The [showOutAnimationDuration] argument is used to specify duration of
+/// enter transition
+///
+/// The [hideOutAnimationDuration] argument is used to specify duration of
+/// exit transition
+///
+/// The [displayDuration] argument is used to specify duration displaying
+///
+/// The [additionalTopPadding] argument is used to specify amount of top
+/// padding that will be added for SafeArea values
+///
+/// The [onTap] callback of [TopSnackBar]
+///
+/// The [overlayState] argument is used to add specific overlay state.
+/// If you will not pass it, it will try to get the current overlay state from
+/// passed [BuildContext]
+void showTopSnackBar(
+    BuildContext context,
+    Widget child, {
+      Duration showOutAnimationDuration = const Duration(milliseconds: 300),
+      Duration hideOutAnimationDuration = const Duration(milliseconds: 200),
+      Duration displayDuration = const Duration(milliseconds: 2000),
+      double additionalTopPadding = 16.0,
+      VoidCallback? onTap,
+      OverlayState? overlayState,
+      double leftPadding = 16,
+      double rightPadding = 16,
+    }) async {
+  overlayState ??= Overlay.of(context);
+  late OverlayEntry overlayEntry;
+  overlayEntry = OverlayEntry(
+    builder: (context) {
+      return TopSnackBar(
+        child: child,
+        onDismissed: () {
+          overlayEntry.remove();
+          _previousEntry = null;
+        },
+        showOutAnimationDuration: showOutAnimationDuration,
+        hideOutAnimationDuration: hideOutAnimationDuration,
+        displayDuration: displayDuration,
+        additionalTopPadding: additionalTopPadding,
+        onTap: onTap,
+        leftPadding: leftPadding,
+        rightPadding: rightPadding,
+      );
+    },
+  );
+
+  _previousEntry?.remove();
+  overlayState?.insert(overlayEntry);
+  _previousEntry = overlayEntry;
+}
+
+/// Widget that controls all animations
+class TopSnackBar extends StatefulWidget {
+  final Widget child;
+  final VoidCallback onDismissed;
+  final showOutAnimationDuration;
+  final hideOutAnimationDuration;
+  final displayDuration;
+  final additionalTopPadding;
+  final VoidCallback? onTap;
+  final double leftPadding;
+  final double rightPadding;
+
+  TopSnackBar({
+    Key? key,
+    required this.child,
+    required this.onDismissed,
+    required this.showOutAnimationDuration,
+    required this.hideOutAnimationDuration,
+    required this.displayDuration,
+    required this.additionalTopPadding,
+    this.onTap,
+    this.leftPadding = 16,
+    this.rightPadding = 16,
+  }) : super(key: key);
+
+  @override
+  _TopSnackBarState createState() => _TopSnackBarState();
+}
+
+class _TopSnackBarState extends State<TopSnackBar>
+    with SingleTickerProviderStateMixin {
+  late Animation offsetAnimation;
+  late AnimationController animationController;
+  double? topPosition;
+
+  @override
+  void initState() {
+    topPosition = widget.additionalTopPadding;
+    _setupAndStartAnimation();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  void _setupAndStartAnimation() async {
+    animationController = AnimationController(
+      vsync: this,
+      duration: widget.showOutAnimationDuration,
+      reverseDuration: widget.hideOutAnimationDuration,
+    );
+
+    Tween<Offset> offsetTween = Tween<Offset>(
+      begin: Offset(0.0, -1.0),
+      end: Offset(0.0, 0.0),
+    );
+
+    offsetAnimation = offsetTween.animate(
+      CurvedAnimation(
+        parent: animationController,
+        curve: Curves.ease,
+        reverseCurve: Curves.linearToEaseOut,
+      ),
+    )..addStatusListener((status) async {
+      if (status == AnimationStatus.completed) {
+        await Future.delayed(widget.displayDuration);
+        if (mounted) {
+          animationController.reverse();
+          setState(() {
+            topPosition = 0;
+          });
+        }
+      }
+
+      if (status == AnimationStatus.dismissed) {
+        widget.onDismissed.call();
+      }
+    });
+
+    if (mounted) {
+      animationController.forward();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedPositioned(
+      duration: widget.hideOutAnimationDuration * 1.5,
+      curve: Curves.ease,
+      top: topPosition,
+      left: widget.leftPadding,
+      right: widget.rightPadding,
+      child: SlideTransition(
+        position: offsetAnimation as Animation<Offset>,
+        child: SafeArea(
+          child: TapBounceContainer(
+            onTap: () {
+              if (mounted) {
+                // widget.onTap?.call();
+                // animationController.reverse();
+              }
+            },
+            child: widget.child,
+          ),
+        ),
+      ),
+    );
+  }
+}
